8b7d36a0d52d223c7d9019a052e3d3fb27653a3c
ManonP <paquereau.emilie.manon@hotmail.fr>
2015-03-29 12:48:26 +0200

    Ajout des méthodes pour l'affichage de l'historique des positions de P et F au cours du jeu. Ajout des méthodes pour afficher les anciennes positions de P ou F au clik sur les coordonnées.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,9 @@ MainWindow::MainWindow(Poursuivant& p, Fuyard& f,QWidget *parent) :
     ui->setupUi(this);
     QObject::connect(ui->boutonTourSuiv,SIGNAL(clicked()),this,SLOT(tourSuivant()));
     QObject::connect(ui->boutonFinirPartie,SIGNAL(clicked()),this,SLOT(terminerPartie()));
+    QObject::connect(ui->boutonQuitter,SIGNAL(clicked()),this,SLOT(close()));
+    QObject::connect(ui->histoP,SIGNAL(clicked(QModelIndex)),this,SLOT(clickOnHistoP()));
+    QObject::connect(ui->histoF,SIGNAL(clicked(QModelIndex)),this,SLOT(clickOnHistoF()));
     this->lancerJeu();
 }
 
@@ -25,11 +28,13 @@ void MainWindow::lancerJeu()
 }
 
 void MainWindow::tourSuivant(){
+    majHisto();
     jeu.tourSuivant();
     majPositionGrille();
 }
 
 void MainWindow::terminerPartie(){
+    majHisto();
     jeu.terminerPartie();
     majPositionGrille();
 }
@@ -67,7 +72,51 @@ void MainWindow::majPositionGrille(){
     }
 }
 
-void MainWindow::on_boutonQuitter_clicked()
+void MainWindow::majHisto(){
+    Historique histo = jeu.getHistorique();
+    Position lastPosP= histo.getDernierePositionPoursuivant();
+    Position lastPosF=histo.getDernierePositionFuyard();
+
+    int xP=lastPosP.getX(); int yP=lastPosP.getY();
+    int xF=lastPosF.getX();int yF=lastPosF.getY();
+
+    QString dernierePositionP="X:"+ QString::number(xP)+ ", Y:"+ QString::number(yP);
+    ui->histoP->addItem(dernierePositionP);
+    QString dernierePositionF="X:"+ QString::number(xF)+ ", Y:"+ QString::number(yF);
+    ui->histoF->addItem(dernierePositionF);
+}
+
+void MainWindow::clickOnHistoP()
 {
-    close();
+     //Récupération de l'index sur lequel l'utilisateur click
+    int currentIndex=ui->histoP->currentRow();
+    QString texte = ui->histoP->item(currentIndex)->text();
+    //Appel de la méthode pour l'affichage avec le texte et une couleur
+    affichePositionHisto(texte,(QColor(255, 0, 0, 100)));
+}
+
+void MainWindow::clickOnHistoF()
+{
+    //Récupération de l'index sur lequel l'utilisateur click
+    int currentIndex=ui->histoF->currentRow();
+    QString texte = ui->histoF->item(currentIndex)->text();
+    //Appel de la méthode pour l'affichage avec le texte et une couleur
+    affichePositionHisto(texte,QColor(0,0,255,100));
+}
+
+void MainWindow::affichePositionHisto(QString texte, QColor color){
+
+    //Récupération du X
+    QString x0=texte.section(':',1,1);
+    QString x1=x0.section(",",0,0);
+
+    //Récupération du Y
+    QString y0=texte.section(':',2,2);
+    QString y1=y0.section(",",0,0);
+
+    //Affichage de la position demandé
+    ui->grille->clearContents(); //On efface la grille
+    majPositionGrille(); //On réaffiche les positions actuelles de P et F
+    ui->grille->setItem(x1.toInt(),y1.toInt(),new QTableWidgetItem);
+    ui->grille->item(x1.toInt(),y1.toInt())->setBackground(color);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -31,7 +31,6 @@ private:
     Jeu jeu;
 
 private slots:
-    void on_boutonQuitter_clicked();
     /*!
         *  \brief Passer au prochain tour
         *
@@ -54,6 +53,33 @@ private slots:
         *
         */
     void majPositionGrille();
+    /*!
+        *  \brief Mettre à jour l'affichage de l'historique
+        *
+        *  Methode qui permet de mettre à jour l'affichage de l'historique
+        * au fur et à mesure que le jeu se déroule
+        */
+    void majHisto();
+    /*!
+        *  \brief Intialise les paramètres pour l'affichage d'une ancienne position de P
+        *
+        *  Methode qui permet d'initialiser les paramètres pour l'affichage d'un ancienne position de P lorsque
+        * l'utilisateur click dessus.
+        */
+    void clickOnHistoP();
+    /*!
+        *  \brief Intialise les paramètres pour l'affichage d'une ancienne position de F
+        *
+        *  Methode qui permet d'initialiser les paramètres pour l'affichage d'un ancienne position de F lorsque
+        * l'utilisateur click dessus
+        */
+    void clickOnHistoF();
+    /*!
+        *  \brief Afficher une ancienne position de F et P
+        *
+        *  Methode qui permet d'afficher une ancienne position de F ou de P sur la grille
+        */
+    void affichePositionHisto(QString texte,QColor color);
 };
 
 #endif // MAINWINDOW_H
